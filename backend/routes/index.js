a8332398b51fd6b763b505f40105f4fd9982f3fa
Chrysanti Anastasya Silaban <chrysanti.silaban@binus.ac.id>
2023-05-01 13:04:39 +0700

    route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/UserController.js";
+import { getKaryawanById, getKaryawans, updateKaryawan, deleteKaryawan, createKaryawan } from "../controllers/KaryawanController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
@@ -10,6 +11,11 @@ router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+router.get('/karyawan', getKaryawans);
+router.get('/karyawan/:id', getKaryawanById);
+router.post('/karyawan', createKaryawan);
+router.patch('/karyawan/:id', updateKaryawan);
+router.delete('/karyawan/:id', deleteKaryawan);
 
 
 export default router;
