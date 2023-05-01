22ada969d1c9ccfdea9ac952b74992c47f177ee6
Chrysanti Anastasya Silaban <chrysanti.silaban@binus.ac.id>
2023-05-01 17:43:32 +0700

    fe route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import KaryawanList from "./components/KaryawanList";
 import AbsenList from "./components/AbsenList";
-
+import AddKaryawan from "./components/AddKaryawan";
+import EditKaryawan from "./components/EditKaryawan";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<><Navbar /><Dashboard /></>} />
         <Route path="/absensi" element={<><Navbar /><AbsenList /></>} />
+        <Route path="/karyawan" element={<><Navbar /><KaryawanList /></>} />
+        <Route path="/karyawan/add" element={<><Navbar /><AddKaryawan /></>} />
+        <Route path="/karyawan/edit/:id" element={<><Navbar /><EditKaryawan /></>} />
       </Routes>
     </BrowserRouter>
   );
