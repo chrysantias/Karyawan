e53ac1f45cd6abf55cd1aa0239668f4eb1c98a80
Chrysanti Anastasya Silaban <chrysanti.silaban@binus.ac.id>
2023-05-01 12:51:59 +0700

    route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers, Register, Login, Logout } from "../controllers/UserKaryawanController.js";
+import { getUsers, Register, Login, Logout } from "../controllers/UserController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
